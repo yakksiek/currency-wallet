b0e2081c2c381ae2f627adce7e75e0351bf864f1
MarcinK <marcin.kulbicki@gmail.com>
2023-10-28 22:25:43 +0200

    create custom useSelect hook

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,10 +1,10 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable no-case-declarations */
-/* eslint-disable no-shadow */
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect, useRef, createRef } from 'react';
 import { UilSearch, UilDirection, UilTimesCircle } from '@iconscout/react-unicons';
+
+import useSelect from '../../hooks/useSelect';
 
 import {
     StyledCustomSelect,
@@ -17,100 +17,134 @@ import {
 } from './Select.styled';
 
 function Select({ optionsList, placeholder, handleChange, name, value }) {
-    const [selectedValue, setselectedValue] = useState(placeholder);
-    const [optionsArr, setOptionsArr] = useState(optionsList);
-    const [inputValue, setInputValue] = useState('');
-    const [listVisible, setListVisible] = useState(false);
-    const [highlightedIndex, setHeighlitedIndex] = useState(0);
-    const optionRefs = useRef([]);
-    const selectContainerRef = useRef(null);
+    const {
+        selectedValue,
+        optionsArr,
+        inputValue,
+        listVisible,
+        highlightedIndex,
+        optionRefs,
+        handleOver,
+        handleClick,
+        handleKeyDown,
+        selectOption,
+        handleInputChange,
+        handleResetValue,
+        selectContainerRef,
+    } = useSelect(optionsList, placeholder, value);
 
-    useEffect(() => {
-        const handleClickOutside = (e) => {
-            if (selectContainerRef.current && !selectContainerRef.current.contains(e.target)) {
-                setListVisible(false);
-            }
-        };
+    // const [selectedValue, setSelectedValue] = useState(placeholder);
+    // const [optionsArr, setOptionsArr] = useState(optionsList);
+    // const [inputValue, setInputValue] = useState('');
+    // const [listVisible, setListVisible] = useState(false);
+    // const [highlightedIndex, setHeighlitedIndex] = useState(0);
+    // const optionRefs = useRef([]);
+    // const selectContainerRef = useRef(null);
 
-        document.addEventListener('click', handleClickOutside);
-        return () => {
-            document.removeEventListener('click', handleClickOutside);
-        };
-    }, []);
+    // useEffect(() => {
+    //     const handleClickOutside = (e) => {
+    //         if (selectContainerRef.current && !selectContainerRef.current.contains(e.target)) {
+    //             setListVisible(false);
+    //         }
+    //     };
 
-    useEffect(() => {
-        if (selectedValue === placeholder) return;
+    //     document.addEventListener('click', handleClickOutside);
+    //     return () => {
+    //         document.removeEventListener('click', handleClickOutside);
+    //     };
+    // }, []);
 
-        // handleChange(name, selectedValue);
-        console.log(selectedValue);
-    }, [selectedValue]);
+    // useEffect(() => {
+    //     if (selectedValue === placeholder) return;
 
-    useEffect(() => {
-        if (listVisible) setHeighlitedIndex(0);
-    }, [listVisible]);
+    //     // handleChange(name, selectedValue);
+    //     console.log(selectedValue);
+    // }, [selectedValue]);
 
-    useEffect(() => {
-        if (value === '') {
-            setselectedValue(placeholder);
-        }
-    }, [value]);
+    // useEffect(() => {
+    //     if (listVisible) setHeighlitedIndex(0);
+    // }, [listVisible]);
 
-    useEffect(() => {
-        const scrollIntoView = () => {
-            if (highlightedIndex >= 0 && optionRefs.current[highlightedIndex]) {
-                optionRefs.current[highlightedIndex].current.scrollIntoView({
-                    behavior: 'auto',
-                    block: 'nearest',
-                    inline: 'nearest',
-                });
-            }
-        };
+    // useEffect(() => {
+    //     if (value === '') {
+    //         setSelectedValue(placeholder);
+    //     }
+    // }, [value]);
 
-        scrollIntoView();
-    }, [highlightedIndex]);
+    // useEffect(() => {
+    //     const scrollIntoView = () => {
+    //         if (highlightedIndex >= 0 && optionRefs.current[highlightedIndex]) {
+    //             optionRefs.current[highlightedIndex].current.scrollIntoView({
+    //                 behavior: 'auto',
+    //                 block: 'nearest',
+    //                 inline: 'nearest',
+    //             });
+    //         }
+    //     };
 
-    const handleOver = (index) => {
-        setHeighlitedIndex(index);
-    };
+    //     scrollIntoView();
+    // }, [highlightedIndex]);
 
-    const handleClick = (e) => {
-        if (e.target.tagName === 'INPUT') return;
-        setListVisible((prevState) => !prevState);
-    };
+    // const handleOver = (index) => {
+    //     setHeighlitedIndex(index);
+    // };
 
-    const handleKeyDown = (e) => {
-        switch (e.code) {
-            case 'Space':
-                setListVisible((prevState) => !prevState);
-                break;
-            case 'ArrowUp':
-            case 'ArrowDown':
-                if (!listVisible) {
-                    setListVisible(true);
-                    break;
-                }
+    // const handleClick = (e) => {
+    //     if (e.target.tagName === 'INPUT') return;
+    //     setListVisible((prevState) => !prevState);
+    // };
 
-                const newValue = highlightedIndex + (e.code === 'ArrowDown' ? 1 : -1);
-                if (newValue >= 0 && newValue < optionsArr.length) {
-                    setHeighlitedIndex(newValue);
-                }
-                break;
-            case 'Enter':
-                setselectedValue(optionsArr[highlightedIndex]);
-                setListVisible(false);
-                break;
-            case 'Escape':
-                setListVisible(false);
-                break;
-            default:
-        }
-    };
+    // const handleKeyDown = (e) => {
+    //     const newValue = highlightedIndex + (e.code === 'ArrowDown' ? 1 : -1);
+    //     switch (e.code) {
+    //         case 'Space':
+    //             setListVisible((prevState) => !prevState);
+    //             break;
+    //         case 'ArrowUp':
+    //         case 'ArrowDown':
+    //             if (!listVisible) {
+    //                 setListVisible(true);
+    //                 break;
+    //             }
+    //             if (newValue >= 0 && newValue < optionsArr.length) {
+    //                 setHeighlitedIndex(newValue);
+    //             }
+    //             break;
+    //         case 'Enter':
+    //             setSelectedValue(optionsArr[highlightedIndex]);
+    //             setListVisible(false);
+    //             break;
+    //         case 'Escape':
+    //             setListVisible(false);
+    //             break;
+    //         default:
+    //     }
+    // };
 
-    const selectOption = (e, name) => {
-        e.stopPropagation();
-        setselectedValue(name);
-        setListVisible(false);
-    };
+    // const selectOption = (e, nameValue) => {
+    //     e.stopPropagation();
+    //     setSelectedValue(nameValue);
+    //     setListVisible(false);
+    // };
+
+    // const filterOptions = (query) => {
+    //     const filteredOptions = optionsList.filter((item) => item.includes(query));
+    //     setOptionsArr(filteredOptions);
+    // };
+
+    // const handleInputChange = (e) => {
+    //     const query = e.target.value.toUpperCase();
+    //     setInputValue(query);
+    //     filterOptions(query);
+    // };
+
+    // const handleResetValue = (e) => {
+    //     e.stopPropagation();
+    //     setInputValue('');
+    //     if (optionsArr.length !== optionsList.length) {
+    //         setOptionsArr(optionsList);
+    //     }
+    // };
 
     const renderOptions = (itemsArr) =>
         itemsArr.map((item, index) => {
@@ -130,27 +164,8 @@ function Select({ optionsList, placeholder, handleChange, name, value }) {
             );
         });
 
-    const filterOptions = (query) => {
-        const filteredOptions = optionsList.filter((item) => item.includes(query));
-        setOptionsArr(filteredOptions);
-    };
-
-    const handleInputChange = (e) => {
-        const query = e.target.value.toUpperCase();
-        setInputValue(query);
-        filterOptions(query);
-    };
-
-    const handleResetValue = (e) => {
-        e.stopPropagation();
-        setInputValue('');
-        if (optionsArr.length !== optionsList.length) {
-            setOptionsArr(optionsList);
-        }
-    };
-
-    const renderInputIcon = (inputValue) =>
-        inputValue === '' ? (
+    const renderInputIcon = (input) =>
+        input === '' ? (
             <span>
                 <UilSearch />
             </span>
@@ -174,7 +189,7 @@ function Select({ optionsList, placeholder, handleChange, name, value }) {
             </StyledValue>
             <StyledListContainer $listVisible={listVisible}>
                 <StyledInputContainer>
-                    <StyledInput placeholder="search" onChange={handleInputChange} value={inputValue} />
+                    <StyledInput tabIndex="0" placeholder="search" onChange={handleInputChange} value={inputValue} />
                     {renderInputIcon(inputValue)}
                 </StyledInputContainer>
                 <StyledSelectOptions $listVisible={listVisible}>{renderOptions(optionsArr)}</StyledSelectOptions>
