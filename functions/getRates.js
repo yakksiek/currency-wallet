a2e7a8d18621b755a1218aecefd5a99430bfad98
MarcinK <marcin.kulbicki@gmail.com>
2024-01-20 12:12:29 +0100

    style(Global) change accent color

--- functions/getRates.js
+++ functions/getRates.js
@@ -1,51 +1,6 @@
 /* eslint-disable no-return-await */
 /* eslint-disable arrow-body-style */
 
-
-// const fetch = require('node-fetch');
-
-// const requestOptions = {
-//     method: 'GET',
-//     redirect: 'follow',
-//     headers: { apiKey: process.env.API_KEY },
-// };
-
-// const dataRates = {
-//     success: true,
-//     timestamp: 1705494843,
-//     base: 'EUR',
-//     date: '2024-01-17',
-//     rates: {
-//         USD: 1.087459,
-//         PLN: 4.395905,
-//         GBP: 0.857483,
-//     },
-// };
-
-// // const url = `http://api.exchangeratesapi.io/v1/latest?access_key=${process.env.API_KEY}&base=EUR&symbols=USD,PLN,GBP`;
-// // const url = `http://api.exchangeratesapi.io/v1/latest?access_key=${process.env.API_KEY}&base=EUR`;
-// const url = `https://api.apilayer.com/exchangerates_data`;
-
-// exports.handler = async (event, context) => {
-//     // const symbols = event.queryStringParameters.symbols || 'USD,PLN,GBP';
-//     // const date = event.queryStringParameters.date || 'latest';
-
-//     // return await fetch(`${this.url}/${date}?symbols=${symbols}&base=EUR`, requestOptions)
-//     //     .then((resp) => {
-//     //         return resp.json();
-//     //     })
-//     //     .then((data) => {
-//     //         console.log(data);
-//     //         return {
-//     //             statusCode: 200,
-//     //             headers: { 'Content-Type': 'application/json' },
-//     //             body: JSON.stringify(data),
-//     //         };
-//     //     });
-
-//     return { statusCode: 200, headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(dataRates) };
-// };
-
 const fetch = require('node-fetch');
 
 const dataRates = {
@@ -59,6 +14,12 @@ const dataRates = {
         GBP: 0.857483,
     },
 };
+
+// const requestOptions = {
+//     method: 'GET',
+//     redirect: 'follow',
+//     headers: { apiKey: process.env.API_KEY },
+// };
 
 // const url = `http://api.exchangeratesapi.io/v1/latest?access_key=${process.env.API_KEY}&base=EUR&symbols=USD,PLN,GBP`;
 const url = `http://api.exchangeratesapi.io/v1/latest?access_key=${process.env.API_KEY_RATES}&base=EUR`;
